263ed5ee5025b810d16778dcb487e15f93cf061e
Michael O%27Brien <mobrien@colgate.edu>
2018-04-25 16:58:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,5 +7,6 @@ var chocolateBars = [
   
 function addElementToBeginningOfArray(array, element) {
   const array1 = ['${array}']
-  const newArray = ['${element}', ...'${array1}']
+  const newArray = ['${element}', ...array1]
+  console.log(newArray)
 }
